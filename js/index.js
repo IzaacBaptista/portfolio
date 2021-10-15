5b5f277f1f0ca46778e6ade0770f1e0de581ac9b
Izaac Baptista <izaacbaptista@gmail.com>
2021-10-15 12:37:12 -0300

    Grid Layout

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 document.querySelector(".toggle").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+/*
+document.querySelector("header").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle(".toggle span")
+);
+*/
