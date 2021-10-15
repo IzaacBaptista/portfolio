0246db24879652003781f673c88ea2a68f608bd9
Izaac Baptista <izaacbaptista@gmail.com>
2021-10-15 00:37:28 -0300

    button close

--- js/index.js
+++ js/index.js
@@ -1,10 +1,3 @@
-var toggle = document.querySelector(".toggle");
-var body = document.querySelector("body");
-
-toggle.addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu");
-});
-
-body.addEventListener("click", function(){
-    document.querySelector("show-menu").classList.body(".sidebar");
-});
+document.querySelector(".toggle").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
