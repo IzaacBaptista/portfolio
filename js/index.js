f5ba1192b4af23e51621277145823bd4f6ce4280
Izaac Baptista <izaacbaptista@gmail.com>
2021-10-19 09:32:07 -0300

    update js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-document.querySelector(".toggle").addEventListener("click", () =>
+document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
-
-/*
-document.querySelector("header").addEventListener("click", () =>
-    document.querySelector(".container").classList.toggle(".toggle span")
-);
-*/
